06576f2672be049378d15472be8b43138199c155
adhya <adhya@192.168.1.83>
2025-12-26 18:00:38 +0530

    first commit

---
+++ StudentManagementSystem2/src/main/java/edu/cjc/sms/app/controller/AdminController.java
@@ -0,0 +1,15 @@
+package edu.cjc.sms.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+	
+	@RequestMapping("/")
+	 public String preLogin(){	
+		
+		return "login";
+	}
+
+}
